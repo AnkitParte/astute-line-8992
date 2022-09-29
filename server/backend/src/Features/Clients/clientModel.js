e9eab58f0f78831a0a2bd678b19ff8d381db10bb
Vijay Singh Rathore <rathorevijay995@gmail.com>
2022-09-29 18:07:27 +0530

    Update clientModel.js

--- server/backend/src/Features/Clients/clientModel.js
+++ server/backend/src/Features/Clients/clientModel.js
@@ -4,7 +4,7 @@ const clientSchema = new mongoose.Schema({
     userId: { type:mongoose.Schema.Types.ObjectId, ref:"user", required:true},
     email: { type: String, required: true},
     name: { type: String, required: true },
-    id: { type: Number },
+    
     contactName:{type:String,required:true},
     phoneNumber:{type:String,required:true},
     website:{type:String,required:true},
