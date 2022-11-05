1e16c8995bef69e403b3e6a3750238be724328ef
apoorvachikara <chikara02apoorva@gmail.com>
2022-11-05 20:12:01 +0530

    Never push secret to GitHub

--- server/backend/src/Config/config.js
+++ server/backend/src/Config/config.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
-
+// feedback: fw18_0044 - Never share the secret credentials and push to github
+// use env variables
 function connect() {
     return mongoose.connect(`mongodb+srv://vijay:12345@cluster0.ggexotb.mongodb.net/hellobonsai`);
 }
